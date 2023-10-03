da5241734448ff751a62f4a1a2d776d44d682ac7
Andrey Lesorukov <lesorukov85@gmail.com>
2023-10-03 15:03:46 +0300

    module done

---
+++ scrapper.py
@@ -0,0 +1,31 @@
+"""
+This module will work only if no permission is needed to get the web-page
+"""
+import re
+import requests
+
+
+def scrap(url: str) -> list[str]:
+    """
+    Scrapping a RU phone numbers on web-page
+    :param url: string of full url path to source
+    :return: list of found phone numbers, return [] if none or hidden(by frontend framework event listeners for ex.).
+    """
+    # Masking to regular browser
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0',
+        'Accept': 'text/html',
+      }
+    r = requests.get(url, headers=headers)
+
+    mass = re.findall(r'([+7,8][-, ]?[(]?\d{3}[)]?[-, ]?\d{3}[-, ]?\d{2}[-, ]?\d{2})', r.content.decode('utf-8'))
+
+    # Filtering to unique items
+    result = []
+    for item in mass:
+        if item not in result:
+            result.append(item)
+    return result
+
+
+print(scrap('https://repetitors.info/'))
